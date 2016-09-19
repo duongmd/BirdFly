c9eed3ebe895f801c2f3ae5e2c25f183ddf0e448
duongmd <duongmdllm@gmail.com>
2016-09-19 14:09:30 +0700

    first commit

--- BirdFly/ViewController.swift
+++ BirdFly/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+    var bird = UIImageView()
+    var audioPlayer = AVAudioPlayer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        drawJungle()
+        addBird()
+        flyUpAndDown()
+        playSong()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func drawJungle(){
+        let background = UIImageView(image: UIImage(named: "jungle.jpg"))
+        background.frame = self.view.bounds
+        background.contentMode = .ScaleAspectFill
+        self.view.addSubview(background)
+    }
+
+    func addBird(){
+        bird = UIImageView(frame: CGRectMake(0, 0, 110, 68))
+        bird.animationImages = [UIImage(named:"bird0.png")!,
+                                UIImage(named:"bird1.png")!,
+                                UIImage(named:"bird2.png")!,
+                                UIImage(named:"bird3.png")!,
+                                UIImage(named:"bird4.png")!,
+                                UIImage(named:"bird5.png")!]
+        bird.animationRepeatCount = 0
+        bird.animationDuration = 1
+        bird.startAnimating()
+        self.view.addSubview(bird)
+    }
+    
+    func flyUpAndDown(){
+        UIView.animateWithDuration(4,animations: {self.bird.center = CGPointMake(self.view.bounds.size.width, self.view.bounds.size.height)})
+        {(finished) in
+        
+            self.bird.transform = CGAffineTransformConcat(CGAffineTransformScale(self.bird.transform, -1, 1),CGAffineTransformMakeRotation(0))
+        
+            UIView.animateWithDuration(3,animations: {self.bird.center = CGPointMake(0,self.view.bounds.size.height) })
+            {(finished) in
+            
+            self.bird.transform = CGAffineTransformConcat(CGAffineTransformScale(self.bird.transform, -1, 1),CGAffineTransformMakeRotation(-45))
+                
+            UIView.animateWithDuration(4,animations: {self.bird.center = CGPointMake(self.view.bounds.size.width, 0)})
+            {(finished) in
+                
+            self.bird.transform = CGAffineTransformConcat(CGAffineTransformScale(self.bird.transform, -1, 1),CGAffineTransformMakeRotation(45))
+                
+            UIView.animateWithDuration(3,animations: {self.bird.center = CGPointMake(0, 0)})
+            {(finished) in
+                
+            self.bird.transform = CGAffineTransformIdentity
+            self.flyUpAndDown()
+            }
+            }
+            }
+            }
+    
     }
 
 
+    func playSong() {
+        let filePath =  NSBundle.mainBundle().pathForResource("A+ – Chào Mào Mái Hót", ofType: ".mp3")
+        let url = NSURL(fileURLWithPath: filePath!)
+        audioPlayer = try! AVAudioPlayer(contentsOfURL: url)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+    }
+    
+    
 }
-
---
+++ readMe.md
@@ -0,0 +1,1 @@
+Bird Flying by MD!
